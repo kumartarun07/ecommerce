9a0964ef75a1eb1f5acda348594a29703aa130d2
kumartarun07 <tarunmalviya814@gmail.com>
2024-08-28 16:16:13 +0530

    custom widget added

---
+++ lib/ui_helper/my_custom_widget.dart
@@ -0,0 +1,15 @@
+import 'package:flutter/material.dart';
+
+class MyCustomWidget extends StatefulWidget {
+  const MyCustomWidget({super.key});
+
+  @override
+  State<MyCustomWidget> createState() => _MyCustomWidgetState();
+}
+
+class _MyCustomWidgetState extends State<MyCustomWidget> {
+  @override
+  Widget build(BuildContext context) {
+    return const Placeholder();
+  }
+}
