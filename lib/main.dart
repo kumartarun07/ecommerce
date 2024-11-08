97bc8f4f37965cc772b98219153f9727642ebfae
kumartarun07 <tarunmalviya814@gmail.com>
2024-11-08 13:00:03 +0530

    change

--- lib/main.dart
+++ lib/main.dart
@@ -1,9 +1,3 @@
-import 'package:ecommerce/ui_page/bottom_page.dart';
-import 'package:ecommerce/ui_page/cart_page.dart';
-import 'package:ecommerce/ui_page/detail.dart';
-import 'package:ecommerce/ui_page/login_page.dart';
-import 'package:ecommerce/ui_page/myhome.dart';
-import 'package:ecommerce/ui_page/signup_page.dart';
 import 'package:ecommerce/ui_page/splash_page.dart';
 import 'package:flutter/material.dart';
 import 'package:flutter/widgets.dart';
